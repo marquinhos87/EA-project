eeb34852f788c78fe78d2dbb673c289c8a55fa25
josepereira1 <joseandremp1@gmail.com>
2020-07-06 12:12:31 +0100

    update

--- code/backend/Clients/src/hrclient/TokenGenerate.java
+++ code/backend/Clients/src/hrclient/TokenGenerate.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class TokenGenerate {
     protected static String tokenGenerate() {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
         while (salt.length() < 32) { // length of the random string.
