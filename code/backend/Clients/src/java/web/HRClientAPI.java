c11e7515abb00c7e81b13411d1c3e51542a091f5
josepereira1 <joseandremp1@gmail.com>
2020-07-08 14:19:46 +0100

    correção de erros, loginClient

--- code/backend/Clients/src/java/web/HRClientAPI.java
+++ code/backend/Clients/src/java/web/HRClientAPI.java
@@ -6,10 +6,14 @@
 package web;
 
 import hrclient.ClientAlreadyExistsException;
+import hrclient.ClientDoesNotExistException;
 import hrclient.HRClientFacade;
+import hrclient.InvalidPasswordException;
 import hrclient.JsonKeyInFaultException;
 import hrclient.Utils;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -63,22 +67,54 @@ public class HRClientAPI extends HttpServlet {
         HRClientFacade facade = HRClientFacade.getInstance();
 
         switch (target){
+            case "updateUserToken":
+                try{
+                    facade.updateUserToken(json);
+                }catch (Exception e){
+                    response.getWriter().print(Utils.makeSuccess(404,"Method not implemented yet."));
+                }
+                break;
             case "createClient":
             {
                 try {
                     json = facade.createClient(json);
                 } catch (PersistentException ex) {
                     response.getWriter().print(Utils.makeError(404, "Internal error."));
+                    break;
                 } catch (JsonKeyInFaultException ex) {
                     response.getWriter().print(Utils.makeError(404, "Json key in fault: " + ex.getMessage()));
+                    break;
                 } catch (ClientAlreadyExistsException ex) {
                     response.getWriter().print(Utils.makeError(404, "Client with username " + ex.getMessage() + " already exist."));
+                    break;
                 }
+                response.getWriter().print(Utils.makeSuccess(200,json));
                 break;
             }
+            case "loginClient":
+            {
+                try {
+                    json = facade.loginClient(json);
+                } catch (InvalidPasswordException ex) {
+                    response.getWriter().print(Utils.makeError(404, "Invalid password."));
+                    break;
+                } catch (PersistentException ex) {
+                    response.getWriter().print(Utils.makeError(404, "Internal error."));
+                    break;
+                } catch (JsonKeyInFaultException ex) {
+                    response.getWriter().print(Utils.makeError(404, "Json key in fault: " + ex.getMessage()));
+                    break;
+                } catch (ClientDoesNotExistException ex) {
+                    response.getWriter().print(Utils.makeError(404, "Client with username " + ex.getMessage() + " does not exist."));
+                    break;
+                }
+                response.getWriter().print(Utils.makeSuccess(200,json));
+                break;
+            }
+            default:
+                response.getWriter().print(Utils.makeSuccess(404,"There is no such method."));
+                break;
         }
-        
-        response.getWriter().print(Utils.makeSuccess(200,json));
     }
 
     /**
