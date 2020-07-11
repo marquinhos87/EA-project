1281f18c8f723512383167ba979af8fbd9d5330e
josepereira1 <joseandremp1@gmail.com>
2020-07-11 11:56:43 +0100

    update

--- code/backend/deployment.sh
+++ code/backend/deployment.sh
@@ -2,8 +2,11 @@
 
 HOST_IP=192.168.1.35
 
+#   Clients service
 cp -rf Clients/dist/Clients.war wildfly-12.0.0.Final/standalone/deployments
 
+#   Request service
 
+#   ...
 
 wildfly-12.0.0.Final/bin/standalone.sh -b $HOST_IP
