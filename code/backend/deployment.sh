38f455a0da6751f7d0ed561bac5e55cb01439e2f
josepereira1 <joseandremp1@gmail.com>
2020-07-11 21:35:42 +0100

    script para copiar wars dos projetos

--- code/backend/deployment.sh
+++ code/backend/deployment.sh
@@ -1,18 +1,18 @@
 #/usr/bin/sh
 
-HOST_IP=192.168.1.139
+HOST_IP=192.168.1.53
 
 #   Clients service
-#cp -rf Clients/dist/Clients.war wildfly-12.0.0.Final/standalone/deployments
+cp -rf Clients/dist/Clients.war wildfly-12.0.0.Final/standalone/deployments
 
 #   Request service
-#cp -rf Request/Request/dist/Request.war wildfly-12.0.0.Final/standalone/deployments
+cp -rf Request/Request/dist/Request.war wildfly-12.0.0.Final/standalone/deployments
 
 #   Core service
-cp -rf Core/Core/dist/Core.war wildfly-12.0.0.Final/standalone/deployments
+#cp -rf Core/Core/dist/Core.war wildfly-12.0.0.Final/standalone/deployments
 
 #   GymAtHome service
-cp -rf GymAtHome/GymAtHome/dist/GymAtHome.war wildfly-12.0.0.Final/standalone/deployments
+#cp -rf GymAtHome/GymAtHome/dist/GymAtHome.war wildfly-12.0.0.Final/standalone/deployments
 
 #   PersonalTrainer service
 #cp -rf PersonalTrainer/dist/PersonalTrainer.war wildfly-12.0.0.Final/standalone/deployments
