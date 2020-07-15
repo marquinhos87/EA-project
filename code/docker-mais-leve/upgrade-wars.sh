d5ba9d46136c7ac49874f0a6b0824dce373b7a81
josepereira1 <joseandremp1@gmail.com>
2020-07-15 13:29:15 +0100

    adicionei a configuração mais simples do docker, com apenas um servidor wildfly para testes

---
+++ code/docker-mais-leve/upgrade-wars.sh
@@ -0,0 +1,15 @@
+#/usr/bin/sh
+	# remove old and copy new war
+	rm -rf Clients.war
+	cp -rf ../backend/Clients/dist/Clients.war .
+	rm -rf Core.war
+	cp -rf ../backend/Core/Core/dist/Core.war .
+	rm -rf GymAtHome.war
+	cp -rf ../backend/GymAtHome/GymAtHome/dist/GymAtHome.war .
+	rm -rf Notification.war
+	cp -rf ../backend/Notification/dist/Notification.war .
+	rm -rf PersonalTrainer.war
+	cp -rf ../backend/PersonalTrainer/dist/PersonalTrainer.war .
+	rm -rf Request.war
+	cp -rf ../backend/Request/Request/dist/Request.war .
+	echo "updated sucessfully!"
