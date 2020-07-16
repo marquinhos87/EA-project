802b8791386f846f7bbf3f3e6fa3b6f718cc756a
João Marques <joaomarques8713@gmail.com>
2020-07-17 00:42:25 +0100

    update

--- code/frontend/GymAtHomeFrontend/src/personaltrainerservlets/ClientProfileServlet.java
+++ code/frontend/GymAtHomeFrontend/src/personaltrainerservlets/ClientProfileServlet.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 @WebServlet(name = "ClientProfileServlet", urlPatterns = "/api/v1/GymAtHomeFrontend/ClientProfile")
 public class ClientProfileServlet extends HttpServlet {
 
-    /*private final Gson gson = new GsonBuilder().create();
+    private final Gson gson = new GsonBuilder().create();
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = (String) request.getSession().getAttribute("username");
@@ -26,19 +26,32 @@ public class ClientProfileServlet extends HttpServlet {
         if(username == null || token == null) {
             request.getSession().setAttribute("username",null);
             request.getSession().setAttribute("token",null);
-            Utils.redirect(request,response,"/WEB-INF/Template.jsp","Login", null);
+            request.getSession().setAttribute("userType",null);
+            Utils.forward(request,response,"/WEB-INF/Template.jsp","Login", null);
         }
         else {
-            String clientUsername = (String) request.getAttribute("clientUsername");
+            String clientUsername = request.getParameter("clientUsername");
             JsonObject jo = new JsonObject();
             jo.addProperty("username",username);
             jo.addProperty("token",token);
             jo.addProperty("clientUsername",clientUsername);
 
-            Response responseHttp = Http.post(Utils.SERVER + "getClientProfileByPersonalTrainer",jo.toString());
+            Response responseHttp;
+
+            try {
+                responseHttp = Http.post(Utils.SERVER + "getClientProfileByPersonalTrainer",jo.toString());
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                request.setAttribute("errorMessage", "Não foi possível conectar ao servidor.");
+                Utils.forward(request, response, "/WEB-INF/Template.jsp", "Login", null);
+                return ;
+            }
 
             String body = responseHttp.body().string();
             ResponseJSON responseJSON = gson.fromJson(body,ResponseJSON.class);
+
+            responseHttp.close();
 
             if(responseJSON.status.equals("success")) {
                 jo = responseJSON.data.getAsJsonObject();
@@ -49,7 +62,7 @@ public class ClientProfileServlet extends HttpServlet {
                 request.setAttribute("genre",jo.get("sex").getAsString());
                 request.setAttribute("height",jo.get("height").getAsString());
                 request.setAttribute("weight",jo.get("weight").getAsString());
-                request.setAttribute("imc",jo.get("bci").getAsString());
+                request.setAttribute("bmi",jo.get("BMI").getAsString());
 
                 String tmp = jo.has("waist") ? jo.get("waist").getAsString() : "0";
                 request.setAttribute("waist",tmp);
@@ -69,14 +82,14 @@ public class ClientProfileServlet extends HttpServlet {
                 tmp = jo.has("chest") ? jo.get("chest").getAsString() : "0";
                 request.setAttribute("chest",tmp);
 
-                Utils.redirect(request,response,"/WEB-INF/Template.jsp","ClientProfile",null);
+                Utils.forward(request,response,"/WEB-INF/Template.jsp","ClientProfile",null);
             }
             else {
-                request.setAttribute("errorMessage",responseJSON.msg);
-                Utils.redirect(request,response,"/WEB-INF/Template.jsp",null,null);
+                request.setAttribute("errorMessage","Erro interno.");
+                Utils.forward(request,response,"/WEB-INF/Template.jsp","Login",null);
             }
         }
-    }*/
+    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -88,7 +101,7 @@ public class ClientProfileServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //processRequest(request,response);
+        processRequest(request,response);
     }
 
     /**
@@ -101,6 +114,6 @@ public class ClientProfileServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //processRequest(request,response);
+        processRequest(request,response);
     }
 }
--- code/frontend/GymAtHomeFrontend/web/WEB-INF/personaltrainer/ClientProfile.jsp
+++ code/frontend/GymAtHomeFrontend/web/WEB-INF/personaltrainer/ClientProfile.jsp
@@ -12,77 +12,57 @@
         <div class="row">
             <label><h3>Cliente: ${requestScope.name}</h3></label>
         </div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>Idade: ${requestScope.age}</td>
+                </tr>
+                <tr>
+                    <td>Género: ${requestScope.genre}</td>
+                </tr>
+                <tr>
+                    <td>Email: ${requestScope.email}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-5">
-        <div class="row">
-            <label>Idade: ${requestScope.age}</label>
-        </div>
+<div class="row justify-content-center">
+    <div class="col-md-10">
+        <table>
+            <thead>
+                <tr>
+                    <th style="width: 50%;"><h4>Dados Biométricos</h4></th>
+                    <th style="width: 50%;"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>IMC: ${requestScope.bci}</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Altura (cm): ${requestScope.height}</td>
+                    <td>Gémeo (cm): ${requestScope.twin}</td>
+                </tr>
+                <tr>
+                    <td>Peso (kg): ${requestScope.weight}</td>
+                    <td>Quadricep (cm): ${requestScope.quadricep}</td>
+                </tr>
+                <tr>
+                    <td>Cintura (cm): ${requestScope.waist}</td>
+                    <td>Tricep (cm): ${requestScope.tricep}</td>
+                </tr>
+                <tr>
+                    <td>Peito (cm): ${requestScope.chest}</td>
+                    <td>Pulso (cm): ${requestScope.wrist}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-5">
-        <div class="row">
-            <label>Género: ${requestScope.genre}</label>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-5">
-        <div class="row">
-            <label>Email: ${requestScope.email}</label>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-5">
-        <div class="row">
-            <label><h4>Dados Biométricos</h4></label>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-5">
-        <div class="row">
-            <label>IMC: ${requestScope.bci}</label>
-        </div>
-        <div class="row">
-            <label>Altura (cm): ${requestScope.height}</label>
-        </div>
-        <div class="row">
-            <label>Peso (kg): ${requestScope.weight}</label>
-        </div>
-        <div class="row">
-            <label>Cintura (cm): ${requestScope.waist}</label>
-        </div>
-        <div class="row">
-            <label>Peito (cm): ${requestScope.chest}</label>
-        </div>
-    </div>
-    <div class="col-md-5">
-        <div class="row">
-            <label>Gémeo (cm): ${requestScope.twin}</label>
-        </div>
-        <div class="row">
-            <label>Quadricep (cm): ${requestScope.quadricep}</label>
-        </div>
-        <div class="row">
-            <label>Tricep (cm): ${requestScope.tricep}</label>
-        </div>
-        <div class="row">
-            <label>Pulso (cm): ${requestScope.wrist}</label>
-        </div>
-    </div>
-    <div class="col-md-1"></div>
 </div>
 <div class="row justify-content-end">
     <div class="col-md-3">
-        <button type="submit" formmethod="get" formaction="${pageContext.request.contextPath}\MyRequests" class="btn btn-danger btn-block text-white font-weight-normal border-0 mb-3">Voltar</button>
+        <button type="submit" formmethod="get" formaction="${pageContext.request.contextPath}\MyRequestsPT" class="btn btn-danger btn-block text-white font-weight-normal border-0 mb-3">Voltar</button>
     </div>
 </div>
