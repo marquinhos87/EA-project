8a4e6fe612fc2e7ebb5fc2f9f4d8c09be00a9b98
João Marques <joaomarques8713@gmail.com>
2020-07-16 15:21:35 +0100

    hashPassword

--- code/frontend/GymAtHomeFrontend/src/utils/Utils.java
+++ code/frontend/GymAtHomeFrontend/src/utils/Utils.java
@@ -25,9 +25,22 @@ public class Utils {
     public static final String SERVER = PROTOCOL + "://" + SERVER_URL + ":" + SERVER_PORT + "/" + SERVER_CONTROLLER + "/api/";
 
     public static String hashPassword(String password) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-        return new String(hash);
+        byte[] arr = password.getBytes();
+        MessageDigest shaDigest = MessageDigest.getInstance("SHA-256");
+        shaDigest.reset();
+        //Read file data and update in message digest
+        shaDigest.update(arr, 0, arr.length);
+        //Get the hash’s bytes
+        byte[] bytes = shaDigest.digest();
+        //This bytes[] has bytes in decimal format;
+        //Convert it to hexadecimal format
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<bytes.length; i++)
+        {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        //return complete hash
+        return sb.toString();
     }
 
     public static void forward(HttpServletRequest request, HttpServletResponse response, String path, String page, String action) throws ServletException, IOException {
